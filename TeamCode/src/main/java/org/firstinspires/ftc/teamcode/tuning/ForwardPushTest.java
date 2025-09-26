c89f366dfaabdfd15c1a26a1f8635afa0632ddd1
Benny Nguyen <lamtruongnguyen888@gmail.com>
2025-09-26 14:44:15 -0700

    Added Pinpoint Localizer Info, Ready for inPerTick Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/ForwardPushTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/ForwardPushTest.java
@@ -2,39 +2,33 @@ package org.firstinspires.ftc.teamcode.tuning;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-@TeleOp(name = "ForwardPushTest", group = "tuning")
+@TeleOp(name = "ForwardPushTest")
 public class ForwardPushTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
         // CHANGE THESE NAMES to match your config
-        DcMotorEx par = hardwareMap.get(DcMotorEx.class, "par");   // parallel dead wheel
-        DcMotorEx perp = hardwareMap.get(DcMotorEx.class, "perp"); // perpendicular dead wheel
 
+        GoBildaPinpointDriver driver = hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
         waitForStart();
 
         if (isStopRequested()) return;
-
-        // reset encoders
-        par.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        perp.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        par.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-        perp.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
 
         // Tell driver to physically push the bot forward ~24 inches
         telemetry.addLine("Push the robot straight forward ~24 inches, then stop.");
         telemetry.update();
 
         while (opModeIsActive()) {
-            int parTicks = par.getCurrentPosition();
-            int perpTicks = perp.getCurrentPosition();
+            int YEncoders = driver.getEncoderY();
+            int XEncoders = driver.getEncoderX();
 
-            telemetry.addData("Parallel Ticks", parTicks);
-            telemetry.addData("Perpendicular Ticks", perpTicks);
+            telemetry.addData("Parallel Ticks (X)", XEncoders);
+            telemetry.addData("Perpendicular Ticks (Y)", YEncoders);
             telemetry.update();
         }
     }

68c73f97e1f734e1b32bb6daa7ae8c5b7ced6876
Benny Nguyen <lamtruongnguyen888@gmail.com>
2025-09-27 11:21:48 -0700

    Edited the Roadrunner TestFile

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunnerstuff/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunnerstuff/MecanumDrive.java
@@ -139,8 +139,8 @@ public final class MecanumDrive {
 
             // TODO: reverse encoders if needed
             //   leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
-            rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
-            leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+            leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
 
             this.pose = pose;
         }
